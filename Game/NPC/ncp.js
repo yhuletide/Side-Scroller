7f53f6b6a5da3175d75924692dde8fc00974db6a
Aidan <aidanbab1@gmail.com>
2021-03-17 11:31:15 -0400

    Linked npc.js

--- Game/NPC/ncp.js
+++ Game/NPC/ncp.js
@@ -6,7 +6,10 @@ class Npc {
     this._locY = locY;//Y coord
   }
   function movment(direction)(){//function that will handle movment for all of the NPC it exspects a string of up,down,left, or right
-    if (direction == "up"){this._locY = this._locy +1}//will make the NPC go up
+    if (direction == "up"){this._locY = this._locY +1}//will make the NPC go up
     else if(direction == "down")(this._locY = this.locY -1)//Will make the npc go down
+    else if (direction == "left"){this._locY = this._locX +1}//will make the NPC go left
+    else if(direction == "right")(this._locY = this.locX -1)//Will make the npc go right
+    else {console.log("Wrong input! Exspected left right up or down and got " + direction + "!");}//this 
   }
 }
