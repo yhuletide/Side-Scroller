475dec650edc1516f08796a9994010f79dd729e8
Horny Ass Man <74560164+HornyAssMan@users.noreply.github.com>
2021-04-19 12:03:35 -0400

    Movement, managed images

--- Game/Player/Movement.js
+++ Game/Player/Movement.js
@@ -28,7 +28,52 @@ else if (e.keyCode == 68 && this._xLocation <= c.width -10){//right(d)
   knight.width = 25;
   knight.height = 25;
   ctx.drawImage(knight, 30, 30, 365, 365);
+}
+  function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
 
+  function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+  }
+
+  function moveup() {
+    myGamePiece.speedY -= 0;
+  }
+
+  function movedown() {
+    myGamePiece.speedY += 0;
+  }
+
+  function moveleft() {
+    myGamePiece.speedX -= 5;
+  }
+
+  function moveright() {
+    myGamePiece.speedX += 5;
+  }
+  </script>
+
+  <button onclick="moveup()">UP</button>
+  <button onclick="movedown()">DOWN</button>
+  <button onclick="moveleft()">LEFT</button>
+  <button onclick="moveright()">RIGHT</button>
 }
 
 }
